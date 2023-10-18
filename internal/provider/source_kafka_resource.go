e622fa51836d5620fa4d1474997f781b0020721b
RDmanta <102177959+RDmanta@users.noreply.github.com>
2023-10-18 15:50:53 -0300

    souce_kafka_resource teste

--- internal/provider/source_kafka_resource.go
+++ internal/provider/source_kafka_resource.go
@@ -1,3 +1,21 @@
 //source_kafka_resource
 
 package provider
+
+import (
+	"airbyte/internal/sdk"
+	"airbyte/internal/sdk/pkg/models/operations"
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+func (r *DestinationS3Resource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_source_kafka"
+}
