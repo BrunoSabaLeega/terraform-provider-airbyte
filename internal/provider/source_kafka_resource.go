6d2b21c230a7b83b09883b28cea07c2036d33bde
RDmanta <102177959+RDmanta@users.noreply.github.com>
2023-10-18 16:05:30 -0300

    source_kafka_resource ajuste

--- internal/provider/source_kafka_resource.go
+++ internal/provider/source_kafka_resource.go
@@ -4,18 +4,23 @@ package provider
 
 import (
 	"airbyte/internal/sdk"
-	"airbyte/internal/sdk/pkg/models/operations"
 	"context"
-	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
-func (r *DestinationS3Resource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+var _ datasource.DataSource = &SourceKlarnaDataSource{}
+var _ datasource.DataSourceWithConfigure = &SourceKlarnaDataSource{}
+
+func NewSourceKafkaDataSource() datasource.DataSource {
+	return &SourceKafkaDataSource{}
+}
+
+// SourceKafkaDataSource is the data source implementation.
+type SourceKafkaDataSource struct {
+	client *sdk.SDK
+}
+
+func (r *SourceKafkaDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_source_kafka"
 }
