31570fdf0c4e440347024f8a16bdb17653e16771
BrunoSabaLeega <127132943+BrunoSabaLeega@users.noreply.github.com>
2023-10-20 14:59:38 -0300

    Update source_kafka_resource2.go

--- internal/provider/source_kafka_resource2.go
+++ internal/provider/source_kafka_resource2.go
@@ -3,11 +3,13 @@ package provider
 import (
     "airbyte/internal/sdk"
     "context"
-
+    "log"
     "github.com/hashicorp/terraform-plugin-framework/schema"
     "github.com/hashicorp/terraform-plugin-framework/resource"
     "github.com/hashicorp/terraform-plugin-framework/types"
 )
+
+/*
 
 // KafkaResource defines the resource implementation.
 type KafkaResource struct {
@@ -56,4 +58,80 @@ func (r *KafkaResource) Schema(ctx context.Context, req resource.SchemaRequest, 
             },
         },
     }
+}
+
+func main() {
+    // Configuração do produtor Kafka
+    config := sarama.NewConfig()
+    config.Producer.Return.Successes = true
+
+    // Lista de endereços do broker Kafka
+    brokerList := []string{"localhost:9092"} // Altere para o endereço do seu broker Kafka
+
+    // Cria um produtor Kafka
+    producer, err := sarama.NewSyncProducer(brokerList, config)
+    if err != nil {
+        log.Fatalf("Erro ao criar o produtor Kafka: %v", err)
+    }
+    defer producer.Close()
+
+    // Tópico para o qual você deseja enviar mensagens
+    topic := "meu-topico"
+
+    // Mensagem a ser enviada
+    message := &sarama.ProducerMessage{
+        Topic: topic,
+        Value: sarama.StringEncoder("Esta é uma mensagem de exemplo."),
+    }
+
+    // Envia a mensagem para o tópico
+    _, _, err = producer.SendMessage(message)
+    if err != nil {
+        log.Fatalf("Erro ao enviar a mensagem: %v", err)
+    }
+
+    log.Println("Mensagem enviada com sucesso para o tópico:", topic)
+}
+
+*/
+
+package main
+
+// Define a struct para o seu DataSource
+type KafkaDataSource struct {
+    // Inclua quaisquer campos necessários aqui
+}
+
+func dataSourceKafka() *schema.Resource {
+    return &schema.Resource{
+        ReadContext: dataSourceKafkaRead,
+        Schema: map[string]*schema.Schema{
+            // Defina os atributos do seu DataSource aqui
+            "brokers": {
+                Type:     schema.TypeString,
+                Required: true,
+                Description: "Lista de endereços dos brokers Kafka",
+            },
+            // Outros atributos...
+        },
+    }
+}
+
+func dataSourceKafkaRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+    // Recupere os atributos do DataSource
+    brokers := d.Get("brokers").(string)
+
+    // Realize a lógica para consultar o Kafka
+    // Conecte-se aos brokers Kafka, recupere informações e popule os resultados
+
+    // Salve os resultados no estado
+    d.SetId("unique-id") // Defina um ID único
+    // d.Set("result_attribute", resultData) // Defina os atributos no estado
+
+    return nil
+}
+
+func main() {
+    // Configure o provedor Terraform, incluindo a definição do seu DataSource
+    // ...
 }
